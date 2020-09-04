ab54629a650d8e52d00cfa0702deb2488c87f936
g0thman <g.othman.anis@gmail.com>
2020-09-04 17:33:19 +0100

    added google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,13 +30,24 @@ module.exports = {
     options: {
       extension: ['.md', '.mdx'],
       gatsbyRemarkPlugins: {
-          resolve: 'gatsby-remark-images',
-          options: {
-            maxWidth: 1200,
-          },
+        resolve: 'gatsby-remark-images',
+        options: {
+          maxWidth: 1200,
         },
       },
     },
+  },
+  {
+    resolve: `gatsby-plugin-google-fonts`,
+    options: {
+      fonts: [
+        'roboto mono',
+        'muli\:400,400i,700,700i',
+      ],
+      display: 'swap',
+    },
+  },
+
   ],
 }
 
