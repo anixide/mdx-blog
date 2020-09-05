ba64ee9afbdb10180bee427e9f6af9e1f8f65ce7
g0thman <g.othman.anis@gmail.com>
2020-09-05 14:09:07 +0100

    Initial blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,24 @@ module.exports = {
   plugins: [
 
   {
-    resolve: 'gatsby-source-filesystem',
+    resolve: `gatsby-source-filesystem`,
     options: {
-      name: 'pages',
+      name: `pages`,
       path: `${__dirname}/src/pages`,
       
     }
   },
   {
-    resolve: 'gatsby-source-filesystem',
+    resolve: `gatsby-source-filesystem`,
     options: {
-      name: 'posts',
+      name: `posts`,
       path: `${__dirname}/src/posts`,
     }
   },
   {
-    resolve: 'gatsby-source-filesystem',
+    resolve: `gatsby-source-filesystem`,
     options: {
-      name: 'images',
+      name: `images`,
       path: `${__dirname}/src/images`,
       
     }
@@ -28,25 +28,28 @@ module.exports = {
   `gatsby-plugin-sharp`,
   `gatsby-transformer-sharp`,
   {
-    resolve: 'gatsby-plugin-mdx',
+    resolve: `gatsby-plugin-mdx`,
     options: {
-      extension: ['.md', '.mdx'],
-      gatsbyRemarkPlugins: {
-        resolve: 'gatsby-remark-images',
-        options: {
-          maxWidth: 1200,
+      extension: [`.md`, `.mdx`],
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 1200,
+          },
         },
-      },
+      ]
+      
     },
   },
   {
     resolve: `gatsby-plugin-google-fonts`,
     options: {
       fonts: [
-        'roboto mono',
-        'muli\:400,400i,700,700i',
+        `roboto mono`,
+        `muli\:400,400i,700,700i`,
       ],
-      display: 'swap',
+      display: `swap`,
     },
   },
 
