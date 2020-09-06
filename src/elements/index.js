cf37bceea7b48591c7a5de66728e5f43ab87abce
g0thman <g.othman.anis@gmail.com>
2020-09-06 03:51:32 +0100

    Creating a grid and watching

---
+++ src/elements/index.js
@@ -0,0 +1,11 @@
+import react { Container } from "../components/Container"
+ :
+export * from "./ContainerElements"
+
+export const IndexPage = () => {
+    <container>
+        <h1>Hello world!</h1>
+    </container>
+}
+
+export default Indexpage;
