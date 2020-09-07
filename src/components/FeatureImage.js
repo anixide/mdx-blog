b0d47c2b56430bb723d656c0e40dd759688f4e9f
g0thman <g.othman.anis@gmail.com>
2020-09-07 19:26:36 +0100

    Header image component added

--- src/components/FeatureImage.js
+++ src/components/FeatureImage.js
@@ -5,10 +5,10 @@ import {FeatureImageWrapper} from "../elements"
 
 export const FeatureImage = ({ fixed }) => {
     
-    const data = useStaticQuery(graphql`
-        query {
-            imageSharp(fixed: {originalName: {eq: "office.jpg"}}) {
-                fixed {
+    const data = useStaticQuery(graphql `
+        query{
+            imageSharp(fixed: {originalName: {eq: "office.jpg"}}){
+                fixed(width: 900, height: 450) {
                     ...GatsbyImageSharpFixed
                 }
             }
@@ -19,11 +19,9 @@ export const FeatureImage = ({ fixed }) => {
     return (
         <FeatureImageWrapper>
             <Img fixed={fixed ? fixed : data.imageSharp.fixed} style={{
-                position: "absolute",
+                //position: "absolute",
                 left: 0,
                 top: 0,
-                width: "100%",
-                height: "100%",
             }} alt="Hero image"/>
         </FeatureImageWrapper>
     )
